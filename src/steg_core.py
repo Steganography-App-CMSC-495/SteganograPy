89757a023316708f97a93e44f21554d51a07a08f
Aeleck <adam.beck95@live.com>
2020-04-08 17:15:53 -0400

    Created loadImage function to core and added test of function into util_driver.py

--- src/steg_core.py
+++ src/steg_core.py
@@ -1,6 +1,10 @@
+from PIL import Image
+
+
 def str2binary(string):
     """Returns the binary string of the input string"""
     return ''.join(format(ord(i), 'b').zfill(8) for i in string)
+
 
 def binary2str(binary):
     """Returns the string representatin of the binary data"""
@@ -8,3 +12,12 @@ def binary2str(binary):
     for i in range(0, len(binary), 8):
         str_data += chr(int(binary[i:i+8], 2))
     return str_data
+
+
+def loadImage(imagePath):
+    """ Opens the image at imagePath, compiles
+        the data into a dictionary and returns"""
+    with Image.open(imagePath) as img:
+        return {'width': img.size[0],
+                'height': img.size[1],
+                'pixels': img.load()}
--- src/util_driver.py
+++ src/util_driver.py
@@ -1,7 +1,21 @@
 import steg_core as core
 
 if __name__ == '__main__':
+    print('*** String-Binary conversion testing ***\n')
     test = 'This is a conversion test! With @ 123--987.0'
     test_bin = core.str2binary(test)
-    print('Binary: ' + test_bin)
-    print('Str: ' + core.binary2str(test_bin))
+    test_conv = core.binary2str(test_bin)
+
+    print(f'Test string is {test}')
+    print(f'In binary, that is {test_bin}')
+    print(f'And converted back into Unicode: {test_conv}')
+
+    print('\n*** Image Pixel Extraction testing ***\n')
+
+    data = core.loadImage('./assets/images/pixel_test.png')
+    print(f"Image is {data['width']} pixels wide "
+          f"and {data['height']} pixels high")
+    print('Pixel data:')
+    for x in range(data['width']):
+        for y in range(data['height']):
+            print(data['pixels'][x, y])
