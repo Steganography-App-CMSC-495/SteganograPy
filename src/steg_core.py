23db86a995dfc39ffc7b81d8de25403c009cee47
Aeleck <adam.beck95@live.com>
2020-04-08 21:30:12 -0400

    Added saving images from custom image data with custom test

--- src/steg_core.py
+++ src/steg_core.py
@@ -14,10 +14,23 @@ def binary2str(binary):
     return str_data
 
 
-def loadImage(imagePath):
+def getImageData(imagePath):
     """ Opens the image at imagePath, compiles
         the data into a dictionary and returns"""
     with Image.open(imagePath) as img:
+        data = list(img.getdata())
         return {'width': img.size[0],
                 'height': img.size[1],
-                'pixels': img.load()}
+                'pixels': [[img.getpixel((x, y))
+                            for x in range(img.size[0])]
+                           for y in range(img.size[1])]}
+
+
+def saveImage(data, path='./image.png'):
+    """ Saves an image to disk using data
+    retrieving/modified from loadImage """
+    with Image.new("RGBA", (data['width'], data['height'])) as img:
+        img.putdata([data['pixels'][x][y]
+                     for x in range(data['width'])
+                     for y in range(data['height'])])
+        img.save(path)
--- src/util_driver.py
+++ src/util_driver.py
@@ -1,6 +1,7 @@
 import steg_core as core
 
-if __name__ == '__main__':
+
+def runStringConversionTest():
     print('*** String-Binary conversion testing ***\n')
     test = 'This is a conversion test! With @ 123--987.0'
     test_bin = core.str2binary(test)
@@ -10,12 +11,37 @@ if __name__ == '__main__':
     print(f'In binary, that is {test_bin}')
     print(f'And converted back into Unicode: {test_conv}')
 
+
+def runImagePixelExtractionTest():
     print('\n*** Image Pixel Extraction testing ***\n')
 
-    data = core.loadImage('./assets/images/pixel_test.png')
+    data = core.getImageData('./assets/images/pixel_test.png')
+    printImageData(data)
+    return data
+
+
+def printImageData(data):
     print(f"Image is {data['width']} pixels wide "
           f"and {data['height']} pixels high")
     print('Pixel data:')
     for x in range(data['width']):
         for y in range(data['height']):
-            print(data['pixels'][x, y])
+            print(data['pixels'][x][y])
+
+
+def runImageSavingTest(data):
+    print('\n*** Image Save testing ***\n')
+    data['width'] *= 2
+    data['height'] *= 2
+    data['pixels'] = [[data['pixels'][int(x / 2)][int(y / 2)]
+                       for x in range(data['width'])]
+                      for y in range(data['height'])]
+    printImageData(data)
+    core.saveImage(data, './assets/images/bigger_pixel_test.png')
+
+
+
+if __name__ == '__main__':
+    runStringConversionTest()
+    data = runImagePixelExtractionTest()
+    runImageSavingTest(data)
