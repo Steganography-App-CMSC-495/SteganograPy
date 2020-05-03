d43945d5a62f8a9f910d9426b044b213437d81dd
Aeleck <adam.beck95@live.com>
2020-05-03 16:46:44 -0400

    Added form checking

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,6 +5,20 @@ from io import BytesIO
 
 @app.route('/api/encode', methods=['POST'])
 def encode():
+
+    # validate form entry
+    if not 'image' in request.files:
+        return jsonify(message='No image key in the request files.'), 400
+
+    if not 'message' in request.form:
+        return jsonify(message='No message key in the request form.'), 400
+
+    if len(request.form['message']) == 0:
+        return jsonify(message='Message is empty.'), 400
+
+    if len(request.files['image'].filename) == 0:
+        return jsonify(message='Empty file submitted.'), 400
+
     try:
         image = core.dataToImage(core.evenOddEncryption(
             core.getImageData(request.files['image']), request.form['message']))
