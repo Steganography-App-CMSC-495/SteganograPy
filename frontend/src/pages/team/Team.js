9474c2331e7aa151796d734ac4af2083b53625e6
Ren Lucas <llucas314@gmail.com>
2020-05-10 16:58:01 -0400

    added content to about and team

--- frontend/src/pages/team/Team.js
+++ frontend/src/pages/team/Team.js
@@ -1,7 +1,25 @@
-import { Grid, Typography } from "@material-ui/core";
+import {
+  Grid,
+  Link,
+  styled,
+  Typography,
+  Tooltip,
+  Zoom,
+} from "@material-ui/core";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
 import React from "react";
 import { withPageContainer } from "../../components";
 
+const LinkWrapper = styled(Link)({
+  backgroundColor: "white",
+  borderRadius: 5,
+  padding: 30,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: 200,
+});
 function Team() {
   return (
     <Grid
@@ -15,6 +33,99 @@ function Team() {
         <Typography variant="h4" color="primary">
           Team
         </Typography>
+      </Grid>
+      <Grid container item justify="center" alignItems="center" xs={10}>
+        <Grid
+          item
+          container
+          justify="center"
+          xs={6}
+          style={{ textAlign: "center" }}
+        >
+          <Tooltip
+            TransitionComponent={Zoom}
+            title="Checkout my GitHub!"
+            placement="top"
+          >
+            <LinkWrapper
+              href="https://github.com/Aeleck"
+              target="_blank"
+              rel="noopener"
+            >
+              <PersonPinIcon color="primary" style={{ fontSize: 75 }} />
+
+              <Typography variant="body1">Adam Beck</Typography>
+            </LinkWrapper>
+          </Tooltip>
+        </Grid>
+        <Grid
+          item
+          container
+          justify="center"
+          xs={6}
+          style={{ textAlign: "center" }}
+        >
+          <Tooltip
+            TransitionComponent={Zoom}
+            title="Checkout my GitHub!"
+            placement="top"
+          >
+            <LinkWrapper
+              href="https://github.com/xdresch"
+              target="_blank"
+              rel="noopener"
+            >
+              <PersonPinIcon color="primary" style={{ fontSize: 75 }} />
+
+              <Typography variant="body1">Andrew Schaeffer</Typography>
+            </LinkWrapper>
+          </Tooltip>
+        </Grid>
+        <Grid
+          item
+          container
+          justify="center"
+          xs={6}
+          style={{ textAlign: "center" }}
+        >
+          <Tooltip
+            TransitionComponent={Zoom}
+            title="Checkout my GitHub!"
+            placement="top"
+          >
+            <LinkWrapper
+              href="https://github.com/saram1995"
+              target="_blank"
+              rel="noopener"
+            >
+              <PersonPinIcon color="primary" style={{ fontSize: 75 }} />
+
+              <Typography variant="body1">Sara McConnell</Typography>
+            </LinkWrapper>
+          </Tooltip>
+        </Grid>
+        <Grid
+          item
+          container
+          justify="center"
+          xs={6}
+          style={{ textAlign: "center" }}
+        >
+          <Tooltip
+            TransitionComponent={Zoom}
+            title="Checkout my GitHub!"
+            placement="top"
+          >
+            <LinkWrapper
+              href="https://github.com/llucas314"
+              target="_blank"
+              rel="noopener"
+            >
+              <PersonPinIcon color="primary" style={{ fontSize: 75 }} />
+              <Typography variant="body1">Lorenzo Lucas</Typography>
+            </LinkWrapper>
+          </Tooltip>
+        </Grid>
       </Grid>
     </Grid>
   );
